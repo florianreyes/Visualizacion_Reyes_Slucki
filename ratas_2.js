62b16d70f1efe14a46770a466d6c290e2415dbfe
Florian <florianreyes541@gmail.com>
2023-04-18 20:42:30 -0300

    Add new ponderacion graph

--- ratas_2.js
+++ ratas_2.js
@@ -1,68 +1,67 @@
-const mapaFetch_normalized = d3.json("geo-json-bsas.json");
-const dataFetch_normalized = d3.dsv(
-  ";",
-  "./datasets/delitos_2021_cantidad.csv",
-  d3.autoType
-);
-
-Promise.all([mapaFetch_normalized, dataFetch_normalized]).then(
-  ([mapa, data]) => {
-    mapa.features.forEach((feature) => {
-      const nombreBarrio = feature.properties.BARRIO;
-      // find matching 'BARRIO' in data and add its 'CANTIDAD' property to the feature
-      const barrioData = data.find((d) => d.BARRIO === nombreBarrio);
-      if (barrioData) {
-        feature.properties.PUNTAJE = barrioData.PUNTAJE;
-      } else {
-        // handle cases where a matching 'BARRIO' is not found in data
-        feature.properties.PUNTAJE = 0;
-        console.warn(`No matching data for ${nombreBarrio}.`);
+d3.dsv(";", "./datasets/delitos_denuncias_merge.csv", d3.autoType).then(
+  (data) => {
+    const data2 = [];
+    const pond_data = [
+      "00",
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+    ];
+    // Iterate over the data object
+    for (let i = 0; i <= 10; i++) {
+      for (barrio of data) {
+        // Iterate over the months
+        // Add barrio, month, and count to the data2 array
+        data2.push({
+          barrio: barrio.BARRIO,
+          ponderacion: pond_data[i],
+          count: Number(barrio[`pond_${i}`]) * 100,
+        });
       }
-    });
-    let chartMap = Plot.plot({
-      projection: {
-        type: "mercator",
-        domain: mapa, // Objeto GeoJson a encuadrar
-      },
-      color: {
-        // Quantize continuo (cant. denuncias) -> discreto (cant. colores)
-        type: "quantize",
-        n: 10,
-        scheme: "ylorrd",
-        label: "Cantidad de robos y homicidios cada 1000 personas.",
-        // legend: true,
-      },
+    }
+    console.log(data2);
+    let chart = Plot.plot({
       marks: [
-        Plot.geo(mapa, {
-          fill: (d) => {
-            let nombreBarrio = d.properties.BARRIO;
-            let cantReclamos = d.properties.PUNTAJE;
-            return cantReclamos;
+        Plot.line(data2, {
+          y: "count",
+          x: "ponderacion",
+          z: "barrio",
+          stroke: (d) => {
+            if (d.barrio == "PALERMO") return "red";
+            else if (d.barrio == "BARRACAS") return "blue";
+            else if (d.barrio == "RECOLETA") return "green";
+            else return "black";
           },
-          stroke: "black",
-          // title: d => ${d.properties.BARRIO}\n${d.properties.DENUNCIAS}
+          title: "barrio",
+          strokeWidth: 2,
         }),
-        Plot.text(
-          mapa.features,
-          Plot.centroid({
-            text: (d) => d.properties.BARRIO,
-            fill: "currentColor",
-            stroke: "white",
-            textAnchor: "center",
-            dx: 4,
-            filter: (d) => d.properties.PUNTAJE > 34,
-          })
-        ),
       ],
-      height: 600,
+      height: 400,
       width: 800,
+      marginLeft: 140,
+      marginBottom: 50,
+      marginTop: 30,
+      marginRight: 10,
+      color: {
+        legend: true,
+      },
       style: {
         "background-color": "#e8e8e8",
         "font-size": "14px",
         "font-family": "Supreme",
         color: "black",
       },
+      y: { label: "Barrios ->", line: true },
+      x: { label: "Seguridad vs Tránsito(%) ->", line: true },
     });
-    d3.select("#chart-4").append(() => chartMap);
+    // Agregamos chart al div#chart de index.html
+    d3.select("#chart-4").append(() => chart);
   }
 );
